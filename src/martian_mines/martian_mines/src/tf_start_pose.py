7c464d6472f5526d606a89fd3edf8ecca413284d
GitJackub <jakubxp9248@gmail.com>
2025-05-30 13:55:32 +0000

    fix: remove duplicated figure_finder

--- src/martian_mines/martian_mines/src/tf_start_pose.py
+++ src/martian_mines/martian_mines/src/tf_start_pose.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy, HistoryPolicy
-from tf2_ros import StaticTransformBroadcaster
+from tf2_ros import StaticTransformBroadcaster, TransformBroadcaster
 
 from geometry_msgs.msg import TransformStamped
 
@@ -26,27 +26,30 @@ class TfStartPose(Node):
             self.get_parameter("drone_z_offset").get_parameter_value().double_value
         )
         self.odometry = None
-        self.odometrt_initialized = False
+        self.odometry_initialized = False
         self.subscription = self.create_subscription(
             ENULocalOdometry, "enu_local_odometry", self.odometry_cb, px4_qos
         )
-        self.broadcaster = StaticTransformBroadcaster(self)
+        self.static_broadcaster = StaticTransformBroadcaster(self)
+        self.broadcaster = TransformBroadcaster(self)
 
     def odometry_cb(self, msg):
         self.odometry = msg
-        if not self.odometrt_initialized:
-            self.odometrt_initialized = True
+        self.send_odometry_transform("map", "base_link", "dynamic") # dynamic: map -> base_link
+
+        if not self.odometry_initialized:
+            self.odometry_initialized = True
             self.get_logger().info(
                 f"Odometry received"
             )
-        self.send_transform()
-        self.destroy_subscription(self.subscription)
+            self.send_camera_transform() # static: base_link -> camera_link
+            self.send_odometry_transform("map", "start_pose", "static") # static: map -> start_pose
 
-    def send_transform(self):
+    def send_odometry_transform(self, parent_frame, child_frame_id, type):
         t = TransformStamped()
-        t.header.frame_id = "map"
+        t.header.frame_id = parent_frame
         t.header.stamp = self.get_clock().now().to_msg()
-        t.child_frame_id = "start_pose"
+        t.child_frame_id = child_frame_id
         t.transform.translation.x = float(self.odometry.x)
         t.transform.translation.y = float(self.odometry.y)
         t.transform.translation.z = float(self.odometry.z + self.drone_z_offset)
@@ -56,7 +59,28 @@ class TfStartPose(Node):
         t.transform.rotation.z = float(self.odometry.qz)
         t.transform.rotation.w = float(self.odometry.qw)
 
-        self.broadcaster.sendTransform(t)
+        if type == "static":
+            self.static_broadcaster.sendTransform(t)
+        else:
+            self.broadcaster.sendTransform(t)
+
+    def send_camera_transform(self):
+        t = TransformStamped()
+        t.header.frame_id = "base_link"
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.child_frame_id = "camera_link"
+
+        # hardcoded values
+        t.transform.translation.x = 0.0
+        t.transform.translation.y = 0.0
+        t.transform.translation.z = 0.0
+
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.707
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w =  0.707
+
+        self.static_broadcaster.sendTransform(t)
 
 
 def main(args=None):
--- src/martian_mines/martian_mines/src/utils/bbox_mapper.py
+++ src/martian_mines/martian_mines/src/utils/bbox_mapper.py
@@ -5,15 +5,14 @@ from tf2_geometry_msgs import do_transform_vector3
 from geometry_msgs.msg import Vector3Stamped
 from martian_mines_msgs.msg import BoundingBoxLabeled
 from rclpy.time import Time
-from rclpy.node import Node
 
-class BBoxMapper(Node):
-    def __init__(self, camera_info_msg, base_frame="start_pose", camera_frame="camera_link"):
-        super().__init__('bbox_mapper')
+class BBoxMapper:
+    def __init__(self, camera_info_msg, node, base_frame="start_pose", camera_frame="camera_link"):
+        self.node = node
         self.camera_model = PinholeCameraModel()
         self.camera_model.fromCameraInfo(camera_info_msg)
         self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self.node)
         self.base_frame = base_frame
         self.camera_frame = camera_frame
 
@@ -23,20 +22,21 @@ class BBoxMapper(Node):
     def set_camera_frame(self, camera_frame):
         self.camera_frame = camera_frame
 
-    def get_transform(self, transform_time=Time()):
+    def get_transform(self, transform_time):
         try:
-            transform = self.tf_buffer.lookup_transform(self.base_frame, self.camera_frame, transform_time)
+            transform = self.tf_buffer.lookup_transform(self.base_frame, self.camera_frame, transform_time) # transform time to fix
+            self.node.get_logger().info(f"transform from bbox_mapper: {transform}")
             return transform
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            self.get_logger().warn(f"Failed to get transform: {e}")
+            self.node.get_logger().warn(f"Failed to get transform: {e}")
             return None
 
-    def bbox_to_ground_position(self, bbox: BoundingBoxLabeled, transform_time=Time()):
+    def bbox_to_ground_position(self, bbox: BoundingBoxLabeled, transform_time):
         ray_camera_frame = self.camera_model.projectPixelTo3dRay((bbox.bbox.center.position.x, bbox.bbox.center.position.y))
-        transform = self.get_transform(transform_time)
+        transform = self.get_transform(transform_time) # transform time to fix
         if transform:
             vector = Vector3Stamped()
-            vector.header.stamp = self.get_clock().now().to_msg()
+            vector.header.stamp = transform_time # transform time to fix
             vector.header.frame_id = self.camera_frame
             vector.vector.x = ray_camera_frame[0]
             vector.vector.y = ray_camera_frame[1]
@@ -54,6 +54,6 @@ class BBoxMapper(Node):
             intersection_point = pyrr.geometric_tests.ray_intersect_plane(ray_local_frame, plane)
             if intersection_point is not None:
                 return intersection_point
-            self.get_logger().warn("The line is parallel to the plane, cannot get intersection.")
+            self.node.get_logger().warn("The line is parallel to the plane, cannot get intersection.")
 
         return None
